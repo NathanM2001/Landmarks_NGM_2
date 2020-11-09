e21a8d7f03a7119e47867b2aac6a1d1f2d149b2b
nathanmckenzie <ngm18@acu.edu>
2020-11-09 17:22:07 -0600

    Initial Commit

---
+++ Landmarks_NGM/Landmarks_NGMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_NGMApp.swift
+//  Landmarks_NGM
+//
+//  Created by Nathan Mckenzie on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_NGMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
