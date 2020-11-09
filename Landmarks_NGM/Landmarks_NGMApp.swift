16fdb49def0903b2bd591ca26d3620fb2765855a
nathanmckenzie <ngm18@acu.edu>
2020-11-09 17:51:30 -0600

    Saving all Work

--- Landmarks_NGM/Landmarks_NGMApp.swift
+++ Landmarks_NGM/Landmarks_NGMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Landmarks_NGMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandmarkList()
         }
     }
 }
---
+++ Landmarks_NGM/Models/LandmarkList.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkList.swift
+//  Landmarks_NGM
+//
+//  Created by Nathan Mckenzie on 11/9/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationBarTitle(Text("Landmarks"))
+            
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+
+        }
+    }
+}
+    
